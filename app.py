65a0cd874fe1ef6b8df571cd65c9f84ce03e268d
ruslan2020202 <example@mail.ru>
2024-02-08 12:13:21 +0300

    commit

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask, render_template, request, url_for, redirect, flash
 
-from contract import w3, contract
+from contract import w3, contract, accounts
+
 
 app = Flask(__name__)
-
-users_accounts = contract.functions.getAccounts().call()
 
 
 @app.route('/')
@@ -14,41 +13,48 @@ def home():
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
+    users_accounts = contract.functions.getAccounts().call()
+    users_reg = list(set(accounts).difference(set(users_accounts)))
     if request.method == 'GET':
-        return render_template('signup.html')
+        return render_template('signup.html', users_reg=users_reg)
     elif request.method == 'POST':
+        name = request.form['name']
+        password = request.form['password']
+        address = request.form['address']
+        print(name, password, address)
         try:
-            name = request.form['name']
-            password = request.form['password']
-            address = request.form['address']
             contract.functions.signup(address, name, password).transact({"from": address})
-            return redirect(url_for('login'))
+            return redirect(url_for('login', users_accounts=users_accounts))
         except Exception as e:
             message = 'Error! User already exists'
             print(e)
-            render_template('signup.html', message=message)
+            render_template('signup.html', message=message, users_reg=users_reg)
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    users_accounts = contract.functions.getAccounts().call()
     if request.method == 'GET':
-        return render_template('login.html')
+        return render_template('login.html', users_accounts=users_accounts)
     elif request.method == 'POST':
         address = request.form['address']
         password = request.form['password']
         print(address, password)
         try:
             contract.functions.login(address, password).transact({"from": address})
-            return redirect(url_for('signed'))
+            return redirect(url_for('signed', user=address))
         except Exception as e:
             print(e)
             message = 'Error! Incorrect address or password'
-            return render_template('login.html', message=message)
+            return render_template('login.html', message=message, users_accounts=users_accounts)
 
 
-@app.route('/signed', methods=['GET'])
+@app.route('/MyAccount', methods=['GET', 'POST'])
 def signed():
-    return render_template('signed.html')
+    user = request.args.get('user')
+    name = contract.functions.getInformation(user).call()
+    balance = 100
+    return render_template('signed.html', user=user, name=name, balance=balance)
 
 
 if __name__ == '__main__':
--- contract.py
+++ contract.py
@@ -1,10 +1,11 @@
+import web3
 from web3 import Web3
 
 w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:7545"))
 
 accounts = w3.eth.accounts
 
-contract_address = '0x8C78f6ca7F428c91A1e0288F2372Da15aDe8f8a4'
+contract_address = '0x808eaa52a7073f1bE1E9370131367265e01cf9f4'
 
 contract_abi = [
     {
@@ -73,6 +74,25 @@ contract_abi = [
                 "internalType": "address",
                 "name": "_account",
                 "type": "address"
+            }
+        ],
+        "name": "getInformation",
+        "outputs": [
+            {
+                "internalType": "string",
+                "name": "",
+                "type": "string"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "_account",
+                "type": "address"
             },
             {
                 "internalType": "string",
@@ -112,3 +132,4 @@ contract_abi = [
 ]
 
 contract = w3.eth.contract(address=contract_address, abi=contract_abi)
+
